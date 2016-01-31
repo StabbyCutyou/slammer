9c9a0167f0cfa0d66e11ee18c36be7c3ccc81241
StabbyCutyou <smattkelly@gmail.com>
2016-01-31 11:26:25 -0500

    initial checkin

---
+++ README.md
@@ -0,0 +1,74 @@
+# Slammer
+
+Slammer is a simple utility using Moldova, for load testing a database. You can give it a template SQL query, most likely an INSERT
+statement, and use that to generate a large volume of traffic, each request having a different set of values placed into it. In this way,
+the Slammer makes an excellent tool for massively loading fake data into a database for load testing.
+
+Slammer comes as a binary executable, and accepts lines of SQL in via STDIN, either through a pipe or through io redirection
+
+## Supported Databases
+
+Since Slammer does not generate SQL, it is agnostic to what kind of database is being used.
+
+That said, currently it only has support for databases that are wire-compatible with either
+postgres or mysql. If you know of another sql/db driver you'd like to see supported, please
+open an issue or a PR.
+
+## Works great with Moldova
+
+Slammer was originally designed to receive input from [Moldova](https://github.com/StabbyCutyou/moldova), a lightweight
+template generation utility which is good for generating random data. By using Moldova, you can generate a stream
+of random insert or select statements, which can be fed to the STDIN of Slammer.
+
+And if you're interested in using them together, checkout the [Moldovan Slammer](http://github.com/StabbyCutyou/moldovan_slammer), a quick helper repo to
+demonstrate using them together
+
+## Slammer Command
+
+To use the command, first install
+
+```bash
+go install github.com/StabbyCutyou/slammer
+```
+
+The command accepts several arguments:
+
+* p - How long of a pause to take between each statement. The default is 1 second, and it can be any valid value that parses to a time.Duration.
+* db - Which driver to load. Currently supports "mysql" and "postgres". The default is "mysql".
+* c - The connection string to the database.
+* w - The size of the worker pool. This defaults to 1, and cannot be lower than 1.
+* d - Debug mode. Enable this to see errors logged to STDERR as they happen
+
+## Example
+
+```bash
+slammer -c "root@tcp(127.0.0.1:3306)/my_db" -p 200us -w 2 < mysqlfile
+```
+
+This would provide sample output like the following:
+
+```
+Slammer Status:
+Queries to run: 200
+---- Worker #0 ----
+  Started at 2016-01-29 22:00:44 , Ended at 2016-01-29 22:00:51, took ...
+  Total work: ..., Percentage work: ... , Average work per second: ...
+  Total errors: ... , Percentage errors: ..., Average errors per second: ...
+---- Worker #1 ----
+  Started at 2016-01-29 22:00:44 , Ended at 2016-01-29 22:00:51, took ...
+  Total work: ..., Percentage work: ..., Average work per second: ...
+  Total errors: ... , Percentage errors: ..., Average errors per second: ...
+```
+
+## Roadmap
+
+Support for more databases
+
+Improve the summary at the end to include overall statistics
+
+Sparklines shown in the terminal to demonstrate latency and throughput with real time
+visualizations
+
+# License
+
+Apache v2 - See LICENSE
---
+++ slammer.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	// Load the drivers
+	// MySQL
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+)
+
+type config struct {
+	connString    string
+	db            string
+	pauseInterval time.Duration
+	workers       int
+	debugMode     bool
+}
+
+type result struct {
+	start     time.Time
+	end       time.Time
+	dbTime    time.Duration
+	workCount int
+	errors    int
+}
+
+func main() {
+	cfg, err := getConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := sql.Open(cfg.db, cfg.connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Declare the channel we'll be using as a work queue
+	inputChan := make(chan (string))
+	// Declare the channel that will gather results
+	outputChan := make(chan (result), cfg.workers)
+	// Declare a waitgroup to help prevent log interleaving - I technically do not
+	// need one, but without it, I find there are stray log messages creeping into
+	// the final report. Setting sync() on STDOUT/ERR didn't seem to fix it.
+	var wg sync.WaitGroup
+	wg.Add(cfg.workers)
+
+	// Start the pool of workers up, reading from the channel
+	for i := 0; i < cfg.workers; i++ {
+		// Pass in everything it needs
+		go func(workerNum int, ic <-chan string, oc chan<- result, d *sql.DB, done *sync.WaitGroup, pause time.Duration, debugMode bool) {
+			// Prep the result object
+			r := result{start: time.Now()}
+			for line := range ic {
+				t := time.Now()
+				_, err := db.Exec(line)
+				r.dbTime += time.Since(t)
+				// TODO should this be after the err != nil? It counts towards work attempted
+				// but not work completed.
+				r.workCount++
+				if err != nil {
+					r.errors++
+					if debugMode {
+						log.Printf("Worker #%d: %s - %s", workerNum, line, err.Error())
+					}
+				} else {
+					// Sleep for the configured amount of pause time between each call
+					time.Sleep(pause)
+				}
+			}
+			// Let everyone know we're done, and bail out
+			r.end = time.Now()
+			oc <- r
+			done.Done()
+		}(i, inputChan, outputChan, db, &wg, cfg.pauseInterval, cfg.debugMode)
+	}
+
+	// Warm up error and line so I can use error in the for loop with running into
+	// a shadowing issue
+	err = nil
+	line := ""
+	totalWorkCount := 0
+	// Read from STDIN in the main thread
+	input := bufio.NewReader(os.Stdin)
+	for err != io.EOF {
+		line, err = input.ReadString('\n')
+		if err == nil {
+			// Get rid of any unwanted stuff
+			line = strings.TrimRight(line, "\r\n")
+			// Push that onto the work queue
+			inputChan <- line
+			totalWorkCount++
+		} else if cfg.debugMode {
+			log.Println(err)
+		}
+	}
+
+	// Close the channel, since it's done receiving input
+	close(inputChan)
+	// As I mentioned above, because workers wont finish at the same time, I need
+	// to use a waitgroup, otherwise the output below gets potentially mixed in with
+	// debug or error messages from the workers. The waitgroup semaphore prevents this
+	// even though it probably looks redundant
+	wg.Wait()
+	// Collect all results, report them. This will block and wait until all results
+	// are in
+	fmt.Println("Slammer Status:")
+	fmt.Printf("Queries to run: %d\n", totalWorkCount)
+	for i := 0; i < cfg.workers; i++ {
+		r := <-outputChan
+		workerDuration := r.end.Sub(r.start)
+		fmt.Printf("---- Worker #%d ----\n", i)
+		fmt.Printf("  Started at %s , Ended at %s, Worker time %s, DB time %s\n", r.start.Format("2006-01-02 15:04:05"), r.end.Format("2006-01-02 15:04:05"), workerDuration.String(), r.dbTime)
+		fmt.Printf("  Total work: %d, Percentage work: %f, Average work over DB time: %f\n", r.workCount, float64(r.workCount)/float64(totalWorkCount), float64(r.workCount)/float64(r.dbTime))
+		fmt.Printf("  Total errors: %d , Percentage errors: %f, Average errors per second: %f\n", r.errors, float64(r.errors)/float64(r.workCount), float64(r.errors)/workerDuration.Seconds())
+	}
+
+	// Lets just be nice and tidy
+	close(outputChan)
+}
+
+func getConfig() (*config, error) {
+	p := flag.String("p", "1s", "The time to pause between each call to the database")
+	c := flag.String("c", "", "The connection string to use when connecting to the database")
+	db := flag.String("db", "mysql", "The database driver to load. Defaults to mysql")
+	w := flag.Int("w", 1, "The number of workers to use. A number greater than 1 will enable statements to be issued concurrently")
+	d := flag.Bool("d", false, "Debug mode - turn this on to have errors printed to the terminal")
+	flag.Parse()
+
+	if *c == "" {
+		return nil, errors.New("You must provide a connection string using the -c option")
+	}
+	pi, err := time.ParseDuration(*p)
+	if err != nil {
+		return nil, errors.New("You must provide a proper duration value with -p")
+	}
+
+	if *w <= 0 {
+		return nil, errors.New("You must provide a worker count > 0 with -w")
+	}
+
+	return &config{db: *db, connString: *c, pauseInterval: pi, workers: *w, debugMode: *d}, nil
+}
